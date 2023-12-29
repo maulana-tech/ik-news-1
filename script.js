b02d0d2a3e127e063e5356a93c63e325e3bb4d01
maulana-tech <145218955+maulana-tech@users.noreply.github.com>
2023-12-29 22:04:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Search(keyword) {
+    console.log("Searching for:", keyword);
+    // Implement your search functionality here
+  }
+  
+  document.querySelector('.menuBtn').addEventListener('click', function() {
+    document.querySelector('.mobile').classList.toggle('hidden');
+  });
+  
